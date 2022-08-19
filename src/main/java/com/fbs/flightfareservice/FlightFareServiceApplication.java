3e597019206184be85399bff3d396d4e99d51602
Sharwari17 <sharunimbalkar17@gmail.com>
2022-08-19 23:42:59 +0530

    First commit

---
+++ src/main/java/com/fbs/flightfareservice/FlightFareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fbs.flightfareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFareServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fbs/flightfareservice/FlightFareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbs.flightfareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFareServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
